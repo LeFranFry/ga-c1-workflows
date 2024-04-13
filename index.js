7d23dc76a64f26589693e98e400b3258e13f67d1
LeFranFry <franto93@yahoo.es>
2024-04-13 18:49:10 -0500

    :sparkles: New version with environment

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var pjson = require('./package.json');
 
 var app = express();
 var version = pjson.version;
+var env = process.env.ENVIRONMENT ? process.env.ENVIRONMENT : 'SANDBOX'
 
 app.get('/', function (req, res) {
-    res.send('Simple Web Application is UP with version ' + version );
+    res.send('Simple Web Application is UP with version ' + version + ' in environment ' + env);
 });
 
 app.listen(8081, function () {
-    console.log('Simple Web Application running on port 8081!');
+    console.log('This Simple Web Application ' + env + ' running on port 8081!');
 });
